279a84ee39c91a97dba822976d9334df0939683f
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-09-12 00:10:29 +0500

    Added spaceX title and links to pages

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { Header } from './Header';
+
+describe ('Tests for Header component', () => {
+    afterEach(cleanup)
+
+    it ('Renders without crashing', () => {
+        render(<Header />);
+    })
+
+    it ('Bears SpaceX title', () => {
+        const { getByText } = render(<Header />);
+        expect( getByText(/spacex/i) ).toBeInTheDocument();
+    })
+    
+    it ('Contains buttons for links to other pages', () => {
+        const { getByTestId } = render(<Header />);
+        expect( getByTestId('home-link') ).toBeInTheDocument();
+        expect( getByTestId('launches-link') ).toBeInTheDocument();
+        expect( getByTestId('rockets-link') ).toBeInTheDocument();
+        expect( getByTestId('history-link') ).toBeInTheDocument();
+        expect( getByTestId('about-link') ).toBeInTheDocument();
+    })
+})
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { FaHome, FaHistory } from 'react-icons/fa';
+import { GoRocket } from 'react-icons/go';
+import { AiFillRocket } from 'react-icons/ai';
+import { BsInfoCircleFill } from 'react-icons/bs';
+// Styles
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+// import blueGrey from '@material-ui/core/colors/blueGrey';
+import grey from '@material-ui/core/colors/grey';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    appBar: {
+        backgroundColor: grey[900],
+    },
+    icon: {
+        color: 'white',
+        marginRight: theme.spacing(0.5),
+        marginLeft: theme.spacing(0.5),
+    },
+    btnText: {
+        marginLeft: theme.spacing(1),
+    },
+    linksToolBar: {
+        marginTop: theme.spacing(-2),
+        marginBottom: theme.spacing(-1),
+        display: 'flex',
+        justifyContent: 'center',
+    },
+}));
+
+export const Header = () => {
+    const classes = useStyles();
+    return (
+        <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+                <Typography variant="h4">
+                    SpaceX <span role="img" aria-label="Rocket Icon">🚀</span>
+                </Typography>
+            </Toolbar>
+            <Toolbar className={classes.linksToolBar}>
+                <Button data-testid="home-link" className={classes.icon}>
+                    <FaHome /> 
+                    <Typography variant="button" className={classes.btnText}>
+                        Home
+                    </Typography>
+                </Button>
+                <Button data-testid="launches-link" className={classes.icon}>
+                    <GoRocket /> 
+                    <Typography variant="button" className={classes.btnText}>
+                        Launches
+                    </Typography>
+                </Button>
+                <Button data-testid="rockets-link" className={classes.icon}>
+                    <AiFillRocket /> 
+                    <Typography variant="button" className={classes.btnText}>
+                        Rockets
+                    </Typography>
+                </Button>
+                <Button data-testid="history-link" className={classes.icon}>
+                    <FaHistory /> 
+                    <Typography variant="button" className={classes.btnText}>
+                        History
+                    </Typography>
+                </Button>
+                <Button data-testid="about-link" className={classes.icon}>
+                    <BsInfoCircleFill /> 
+                    <Typography variant="button" className={classes.btnText}>
+                        About
+                    </Typography>
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
