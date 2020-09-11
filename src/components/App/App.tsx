d830e2b91a0894b80784c9913d87838b805f560b
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-09-12 00:11:14 +0500

    Rendered <Header />

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from '../Header/Header';
 
 const App = () => {
   return (
-    <div className="App">
-      Hello, World!
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
