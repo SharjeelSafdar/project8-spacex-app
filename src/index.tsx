cff861011dd59c8e692625e146049e6c9c13d44a
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-09-13 07:25:31 +0500

    Instanciated ApolloClient and added ApolloProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'; 
+
+const client = new ApolloClient({
+  uri: "http://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
