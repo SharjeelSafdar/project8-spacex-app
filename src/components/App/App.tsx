2d7a3d5a40bb7ae04b6488e4d04a8efa5296dc30
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-09-15 23:55:46 +0500

    Removed direct placement of <Timer />

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from '../Header/Header';
 import { Home } from '../Home/Home';
+// import { Timer } from '../Timer/Timer';
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      {/* <Timer secondsLeft={3000}/> */}
     </>
   );
 }
