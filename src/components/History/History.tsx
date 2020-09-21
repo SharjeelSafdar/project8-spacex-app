2e348704a7038c7cfcc6bf823780f1ac6ef801a7
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-09-21 12:30:32 +0500

    Resolved date bug

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -10,7 +10,7 @@ export const History = () => {
     return (
         <div className={styles.container}>
             <h3 className={styles.heading}>History</h3>
-            <div className={styles.histories}  style={{paddingBottom: (loading || !!error) ? '20px' : 0}}>
+            <div className={styles.histories} style={{paddingBottom: (loading || !!error) ? '20px' : 0}}>
                 {loading ? 'Loading data...' : error ? 'Error fetching data...' :
                     data && data.history && data?.history.map( (historyItem, index) => (
                         <div className={styles.historyItem} data-testid={`historyItem${index+1}`} key={index+1}>
@@ -18,7 +18,7 @@ export const History = () => {
                                 {`${historyItem && historyItem.id}: ${historyItem?.title}`}
                             </h4>
                             <p>{`Date: ${historyItem?.event_date_unix && 
-                                new Date(historyItem.event_date_unix).toUTCString()}`
+                                new Date(historyItem.event_date_unix * 1000).toUTCString()}`
                             }</p>
                             {historyItem?.flight_number && <p>{`Flight Number: ${historyItem.flight_number}`}</p>}
                             <p>{`Details: ${historyItem?.details}`}</p>
@@ -27,29 +27,29 @@ export const History = () => {
                                     {!!historyItem?.links?.article &&
                                         <span>
                                             <a
-                                            href={historyItem?.links?.article}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            title="Article Link"
-                                            data-testid={`article${index}`}
-                                            className={styles.articleLink}
-                                        >
-                                            <RiArticleLine />
-                                        </a>
+                                                href={historyItem?.links?.article}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                title="Article Link"
+                                                data-testid={`article${index}`}
+                                                className={styles.articleLink}
+                                            >
+                                                <RiArticleLine />
+                                            </a>
                                         </span>
                                     }
                                     {!!historyItem?.links?.wikipedia &&
                                         <span>
                                             <a
-                                            href={historyItem?.links?.wikipedia}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            title="Wikipedia Link"
-                                            data-testid={`wiki${index}`}
-                                            className={styles.wikiLink}
-                                        >
-                                            <FcWikipedia />
-                                        </a>
+                                                href={historyItem?.links?.wikipedia}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                title="Wikipedia Link"
+                                                data-testid={`wiki${index}`}
+                                                className={styles.wikiLink}
+                                            >
+                                                <FcWikipedia />
+                                            </a>
                                         </span>
                                     }
                                 </p>
